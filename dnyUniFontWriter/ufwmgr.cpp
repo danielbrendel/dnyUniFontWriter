f490a5c6d33cd07f896f3d25ce3297ee6f51f035
Daniel Brendel <dbrendel1988@gmail.com>
2023-07-12 12:02:50 +0200

    Year

--- dnyUniFontWriter/ufwmgr.cpp
+++ dnyUniFontWriter/ufwmgr.cpp
@@ -4,7 +4,7 @@
 /*
 	UniFont Writer (dnyUniFontWriter) developed by Daniel Brendel
 
-	(C) 2018 - 2022 by Daniel Brendel
+	(C) 2018 - 2023 by Daniel Brendel
 
 	Version: 0.1
 	Contact: dbrendel1988<at>gmail<dot>com
